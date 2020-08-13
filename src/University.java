ca38181638b9b29dcdb3218997f1460bc635e163
acho0057 <acho0057@student.monash.edu>
2020-08-13 19:37:19 +0800

    Initial Commit of Week 2 Lab Source Code

---
+++ src/University.java
@@ -0,0 +1,8 @@
+public class University {
+
+    public void printStatus() {
+        System.out.println("Welcome to Java University");
+        System.out.println();
+        System.out.println("Thank you for using Java University");
+    }
+}
---
+++ src/UniversityDriver.java
@@ -0,0 +1,6 @@
+public class UniversityDriver {
+  public static void main(String[] args) {
+    University Monash=new University();
+    Monash.printStatus();
+  }
+}
